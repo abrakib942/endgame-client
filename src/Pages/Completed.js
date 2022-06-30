b589e6c3d7244235929fb6957482194750d86e4e
abrakib942 <abrakib942@gmail.com>
2022-07-01 02:56:41 +0600

    check and complete

--- src/Pages/Completed.js
+++ src/Pages/Completed.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const Completed = () => {
+  const {
+    data: completes,
+    isLoading,
+    refetch,
+  } = useQuery(
+    "completes",
+    fetch("http://localhost:5000/complete").then((res) => res.json())
+  );
+
+  if (isLoading) {
+    <Loading />;
+  }
+
   return (
     <div>
-      <h2 className="text-center text-4xl">Completed Tasks</h2>
+      <h2 className="text-center text-2xl font-bold mt-12 text-accent">
+        Completed Tasks
+      </h2>
     </div>
   );
 };
--- src/Pages/Todo/Task.js
+++ src/Pages/Todo/Task.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const Task = ({ task, refetch, setEditTask }) => {
-  const { text } = task;
+  const { text, _id } = task;
+
+  const handleComplete = () => {
+    const completeTask = {
+      text,
+    };
+
+    fetch("http://localhost:5000/complete", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(completeTask),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          toast("task completed");
+          refetch();
+        }
+      });
+
+    fetch(`http://localhost:5000/todo/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          refetch();
+        }
+      });
+  };
 
   return (
     <tr>
       <td>
-        <input type="checkbox" class="checkbox checkbox-accent" />
+        <input
+          onClick={handleComplete}
+          type="checkbox"
+          class="checkbox checkbox-accent"
+        />
       </td>
       <td>{text}</td>
       <td>
@@ -17,6 +53,7 @@ const Task = ({ task, refetch, setEditTask }) => {
         >
           Edit
         </label>
+
         {/* <button className="btn btn-secondary btn-xs">Delete</button> */}
       </td>
       <td></td>
